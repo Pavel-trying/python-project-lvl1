5039e7127755769ceb968562f90910d2beceb70c
Pavel <kplyft@gmail.com>
2020-07-21 15:52:27 +0300

    work version lesson3

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,5 +1,5 @@
 def welcome_user():
     import prompt
-    name = prompt.string('May I have your name?')
+    name = prompt.string('May I have your name? ')
     template = "{}, {}!"
     print(template.format('Hello', name))
