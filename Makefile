afdeaa138c7931f21aa633018c533cdc2f4914f3
Pavel <kplyft@gmail.com>
2020-07-24 20:16:58 +0300

    i hope it will work)))

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
 install:
 	@poetry install
+	
 lint:
 	poetry run flake8 brain_games
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,12 +1,12 @@
 import sys
-
 sys.path.append("/home/pavel/python-project-lvl1/brain_games")
+from cli import welcome_user # noqa
 
-from cli import welcome_user
 
 def main():
     print('Welcome to the Brain Games!')
     welcome_user()
+
 
 if __name__ == '__main__':
     main()
