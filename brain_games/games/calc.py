79138d4f09907f10e0004b4f08df93cc6d08617b
Pavel <kplyft@gmail.com>
2020-11-22 16:22:27 +0200

    fix calc game

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -5,14 +5,14 @@ DESCRIPTION = 'What is the result of the expression?'
 
 
 def get_question_and_answer():
-    choise_sign = choice('-+*')
+    sign = choice('-+*')
     first_num = randint(25, 50)
     sec_num = randint(1, 25)
-    if choise_sign == ('+'):
+    if sign == ('+'):
         result = first_num + sec_num
-    elif choise_sign == ('-'):
+    elif sign == ('-'):
         result = first_num - sec_num
-    else:
+    elif sign == ('*'):
         result = first_num * sec_num
-    question = f"{str(first_num)} {choise_sign} {str(sec_num)}"
+    question = f"{str(first_num)} {sign} {str(sec_num)}"
     return str(result), question
