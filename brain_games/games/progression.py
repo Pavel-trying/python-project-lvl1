5e3dc5eb6d8ecc95767dd30c1052a307333308f2
Pavel <kplyft@gmail.com>
2020-11-26 06:07:08 +0200

    finnal variant of progression game

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -7,23 +7,16 @@ DESCRIPTION = 'What number is missing in the progression?'
 def get_question_and_answer():
     quantity_of_nums_in_question = 10
     start_num = randint(1, 100)
-    position_of_indefinite_num = randint(1, quantity_of_nums_in_question - 1)
+    position_of_indefinite_num = randint(0, quantity_of_nums_in_question - 1)
     value_of_progression = randint(1, 30)
-    index_of_repeats = 1
-    if position_of_indefinite_num == 1:
-        question = ('..')
-        result = start_num
-        index_of_repeats += 1
-    else:
-        question = str(start_num)
-    while index_of_repeats <= quantity_of_nums_in_question:
-        if position_of_indefinite_num == index_of_repeats:
-            question += (' ..')
-            start_num += value_of_progression
-            index_of_repeats += 2
-            result = start_num
+    index_of_num = 0
+    question = ''
+    while index_of_num < quantity_of_nums_in_question:
+        if index_of_num == position_of_indefinite_num:
+            question += '. . '
+            index_of_num += 1
         else:
-            start_num += value_of_progression
-            question += (' ' + str(start_num))
-            index_of_repeats += 1
-    return str(result), question
+            question += f'{start_num + value_of_progression * index_of_num} '
+            index_of_num += 1
+    result = start_num + value_of_progression * position_of_indefinite_num
+    return str(result), question.rstrip()
