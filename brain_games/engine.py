af5b5bbfae1517a2392412b3a6617ce972dea59c
Pavel <kplyft@gmail.com>
2020-11-29 06:48:06 +0200

    add constant, move return

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -2,21 +2,24 @@ import prompt
 
 
 def run(game):
-    number_of_questions = 3
+    QUANTITY_OF_ROUNDS = 3
+    number_of_round = 1
     print('Welcome to the Brain Games!')
     print(game.DESCRIPTION + '\n')
     name = prompt.string('May I have your name? ')
     welcome = f'Hello, {name}'
     print('\n' + welcome)
-    while number_of_questions != 0:
+    while number_of_round <= QUANTITY_OF_ROUNDS:
         result, question, = game.get_question_and_answer()
         print('Question: ' + question)
         answer = prompt.string('Your answer: ')
         if result == answer:
             print('Correct!')
-            number_of_questions -= 1
+            number_of_round += 1
         else:
             answer_is_wrong = f'''"{answer}" is wrong answer ;(. Correct answer was "{result}"
 Let's try again, {name}!'''
-            return print(answer_is_wrong)
+            print(answer_is_wrong)
+            return
     print('Congratulations, ' + name + '!')
+
